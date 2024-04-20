afd9237fc00bc3484ce7a83adc0991b43229e3d2
KALIMULLAH <165498573+KALIMULLAH41@users.noreply.github.com>
2024-04-20 20:41:03 +0530

    Add files via upload

---
+++ Laptop.java
@@ -0,0 +1,20 @@
+package Laptop;
+
+public class Laptop {
+		int lId ;
+		String brand ;
+		double cost ;
+		void switchon(){	
+		System.out.println("laptop is on");	
+		
+		}
+      void switchoff() {
+    	  
+    	  System.out.println("laptop is of");  
+      }
+    void info() {
+   System.out.println(lId );
+   System.out.println(brand);
+   System.out.println(cost );
+    }
+}
---
+++ LaptopApp1.java
@@ -0,0 +1,24 @@
+package Laptop;
+
+public class LaptopApp1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Laptop l1 = new Laptop();
+		l1.lId= 12993;
+		l1.brand= "Dell";
+		l1.cost = 23131.34;
+		l1.switchon();
+		l1.switchon();
+		l1.info();
+		new Laptop() .info();
+		
+		
+		
+		
+		
+		
+	}
+
+}
